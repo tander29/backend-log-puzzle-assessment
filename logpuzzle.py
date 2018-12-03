5967bdd552cec7bc1c8b096ceefbcd449e7fd5a2
Travis Anderson <thetanderson@gmail.com>
2018-12-02 21:35:22 -0500

    parts a and b

--- logpuzzle.py
+++ logpuzzle.py
@@ -11,7 +11,7 @@ http://code.google.com/edu/languages/google-python-class/
 
 Given an apache logfile, find the puzzle urls and download the images.
 
-Here's what a puzzle url looks like:
+Here's what puzzle_path puzzle url looks like:
 10.254.254.28 - - [06/Aug/2007:00:13:48 -0700] "GET /~foo/puzzle-bar-aaab.jpg HTTP/1.0" 302 528 "-" "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.6) Gecko/20070725 Firefox/2.0.0.6"
 
 """
@@ -24,12 +24,21 @@ import argparse
 
 
 def read_urls(filename):
-    """Returns a list of the puzzle urls from the given log file,
+    """Returns puzzle_path list of the puzzle urls from the given log file,
     extracting the hostname from the filename itself.
     Screens out duplicate urls and returns the urls sorted into
     increasing order."""
     # +++your code here+++
-    pass
+    url = 'http://' + filename[(filename.find('_') + 1):]
+    all_lines = []
+    puzzles_list = []
+    with open(filename) as f:
+        all_lines = f.readlines()
+    for line in all_lines:
+        puzzle_path = re.search(r'\S+(puzzle)\S+', line)
+        if puzzle_path:
+            puzzles_list.append(url + puzzle_path.group(0))
+    return sorted(set(puzzles_list))
 
 
 def download_images(img_urls, dest_dir):
@@ -41,13 +50,29 @@ def download_images(img_urls, dest_dir):
     Creates the directory if necessary.
     """
     # +++your code here+++
-    pass
+    if os.path.isdir(dest_dir):
+        return
+    os.mkdir(dest_dir)
+    os.chdir(dest_dir)
+    images = []
+    for n, path in enumerate(img_urls):
+        urllib.urlretrieve(path, 'img{}.jpg'.format(n))
+        images.append('img{}.jpg'.format(n))
+        print(path)
+    with open('index.html', 'w') as f:
+        f.write('<html><body>\n')
+        for image in images:
+            f.write('<img src="{}">'.format(image))
+        f.write('\n')
+        f.write('</body></html>')
+    print(os.getcwd())
 
 
 def create_parser():
     """Create an argument parser object"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--todir',  help='destination directory for downloaded images')
+    parser.add_argument(
+        '-d', '--todir',  help='destination directory for downloaded images')
     parser.add_argument('logfile', help='apache logfile to extract urls from')
 
     return parser
